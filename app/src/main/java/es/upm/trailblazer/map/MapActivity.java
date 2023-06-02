dfe9207739c094ff5a3b254c348df0b2207b3bc6
dgarcas <david.garcia.castano@alumnos.upm.es>
2023-06-03 00:08:54 +0200

    refactor, now map activity has fragments

--- app/src/main/java/es/upm/trailblazer/map/MapActivity.java
+++ app/src/main/java/es/upm/trailblazer/map/MapActivity.java
@@ -3,6 +3,7 @@ package es.upm.trailblazer.map;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
+import androidx.fragment.app.Fragment;
 
 import android.Manifest;
 import android.content.Context;
@@ -32,90 +33,17 @@ import org.osmdroid.views.overlay.mylocation.GpsMyLocationProvider;
 import org.osmdroid.views.overlay.mylocation.MyLocationNewOverlay;
 
 import es.upm.trailblazer.R;
+import es.upm.trailblazer.TrackFragment;
 
 public class MapActivity extends AppCompatActivity {
 
-    private MapView map;
-    private MyLocationNewOverlay mLocationOverlay;
-    private ScaleBarOverlay mScaleBarOverlay;
-    private RotationGestureOverlay mRotationGestureOverlay;
-    private FloatingActionButton gpsActionButton;
-
+    Fragment trackFragment;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        Context context = getApplicationContext();
-        Configuration.getInstance().load(context, PreferenceManager.getDefaultSharedPreferences(context));
-
         setContentView(R.layout.activity_map);
+        trackFragment = new TrackFragment();
 
-        checkLocationPermissions ();
-        map = findViewById(R.id.map_view);
-        setMapConfigurations();
-        gpsActionButton = findViewById(R.id.floatingActionButton);
-
-        gpsActionButton.setOnClickListener(v -> {
-            if(mLocationOverlay.isFollowLocationEnabled()){
-                mLocationOverlay.disableFollowLocation();
-                gpsActionButton.setImageResource(R.drawable.gps_off);
-            } else {
-                mLocationOverlay.enableFollowLocation();
-                map.getController().setZoom(20);
-                gpsActionButton.setImageResource(R.drawable.gps_fixed);
-            }
-        });
-        map.setOnTouchListener((v, event) -> {gpsActionButton.setImageResource(R.drawable.gps_off);
-            return false;
-        });
-
-    }
-
-    private void checkLocationPermissions (){
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
-                ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION}, 1);
-        }
-
-    }
-
-    private void setMapConfigurations (){
-        final DisplayMetrics dm = this.getResources().getDisplayMetrics();
-
-
-        map.setTileSource(TileSourceFactory.MAPNIK);
-        map.setMultiTouchControls(true);
-        IMapController mapController = map.getController();
-        mapController.setZoom(20);
-
-        mLocationOverlay = new MyLocationNewOverlay(new GpsMyLocationProvider(this), map);
-        mLocationOverlay.enableMyLocation();
-        mLocationOverlay.enableFollowLocation();
-
-        map.getOverlays().add(this.mLocationOverlay);
-
-        //map scale
-        mScaleBarOverlay = new ScaleBarOverlay(map);
-        mScaleBarOverlay.setCentred(true);
-        mScaleBarOverlay.setScaleBarOffset(dm.widthPixels / 2, 10);
-        map.getOverlays().add(this.mScaleBarOverlay);
-
-
-        //support for map rotation
-        mRotationGestureOverlay = new RotationGestureOverlay(map);
-        mRotationGestureOverlay.setEnabled(true);
-        map.getOverlays().add(this.mRotationGestureOverlay);
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        map.onResume();
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-        map.onPause();
+        getSupportFragmentManager().beginTransaction().add(R.id.loginContainer, trackFragment).commit();
     }
 }
